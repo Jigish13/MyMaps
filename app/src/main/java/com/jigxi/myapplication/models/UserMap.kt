28c563ce760a57e53519c7dab19a88295d3ef1a8
Jigish Vyas <jiggy@Jigishs-MacBook-Pro.local>
2022-02-07 12:21:27 -0500

    Added rv on main activity and set onClickListener on it

--- app/src/main/java/com/jigxi/myapplication/models/UserMap.kt
+++ app/src/main/java/com/jigxi/myapplication/models/UserMap.kt
@@ -1,4 +1,4 @@
 package com.jigxi.myapplication.models
 
-class UserMap {
-}
+// it will hold map title and all markers(i.e. places) on it
+data class UserMap(val title:String, val places:List<Place>)
