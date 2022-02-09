b40386e97603c72f11bc190067177d68fca1d933
Jigish Vyas <jiggy@Jigishs-MacBook-Pro.local>
2022-02-09 17:01:17 -0500

    Created alert dialog to create marker in CreateMapsActivity and added SnackBar

--- app/src/main/java/com/jigxi/myapplication/CreateMapsActivity.kt
+++ app/src/main/java/com/jigxi/myapplication/CreateMapsActivity.kt
@@ -1,8 +1,15 @@
 package com.jigxi.myapplication
 
+import android.content.Context
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -11,6 +18,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
 import com.jigxi.myapplication.databinding.ActivityCreateMapsBinding
 
 private const val TAG = "CreateMapsActivity"
@@ -35,6 +43,15 @@ class CreateMapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        // providing user information on how to add marker
+        // if mapFragment is not null, pass it as an object in SnackBar
+        mapFragment.view?.let {
+            Snackbar.make(it, "Long press to add a marker!", Snackbar.LENGTH_INDEFINITE)
+                .setAction("OK", {})
+                .setActionTextColor(ContextCompat.getColor(this, android.R.color.white))
+                .show()
+        }
     }
 
     /**
@@ -57,12 +74,35 @@ class CreateMapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         mMap.setOnMapLongClickListener { latLng ->
             Log.i(TAG, "onMapReady: OnMapLongClickListener")
-            val marker = mMap.addMarker(MarkerOptions().position(latLng).title("new marker").snippet("cool snippet"))
-            markers.add(marker)
+            showAlertDialog(latLng)
         }
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun showAlertDialog(latLng: LatLng) {
+        val placeFormView = LayoutInflater.from(this).inflate(R.layout.dialog_create_place, null)
+        val dialog =
+            AlertDialog.Builder(this)
+            .setTitle("Create a marker")
+            .setView(placeFormView)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("OK", null)
+            .show()
+
+        // add marker only if user has clicked on OK button
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
+            val title = placeFormView.findViewById<EditText>(R.id.etTitle).text.toString()
+            val description = placeFormView.findViewById<EditText>(R.id.etDescription).text.toString()
+            if (title.trim().isEmpty() || description.trim().isEmpty()) {
+                Toast.makeText(this, "Place must not have non empty title or description", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            val marker = mMap.addMarker(MarkerOptions().position(latLng).title(title).snippet(description))
+            markers.add(marker)
+            dialog.dismiss()
+        }
     }
 }
